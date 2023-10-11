3119d205b0d015892b46692d83febbe8069202dd
Shahbaz <rajashayzee@yahoo.com>
2023-10-11 12:37:07 +0500

    gift card search

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -2,109 +2,113 @@
 
 
 @section('title')
-    Card
+Card
 @endsection
 
 @section('css-link')
-    <link rel="stylesheet" href="{{ asset('assets/style/contact.css') }}" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.6.0/pagination.min.css"
-        integrity="sha512-K1k7jSn9RDKEcn/ugqVVvWYu0bcS3q1w6m/5pQSnrj0bCfIqF6Wk49lkmckSb4wglvTP9V17LtS0q0XxYccXbg==" crossorigin="anonymous"
-        referrerpolicy="no-referrer" />
-    <style>
-        button.btn.btn-outline-secondary.filter-card {
-            border-radius: 1px;
-            background: #5570F1;
-            color: white;
-            border: 1px solid #5570F1;
-        }
+<link rel="stylesheet" href="{{ asset('assets/style/contact.css') }}" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.6.0/pagination.min.css"
+    integrity="sha512-K1k7jSn9RDKEcn/ugqVVvWYu0bcS3q1w6m/5pQSnrj0bCfIqF6Wk49lkmckSb4wglvTP9V17LtS0q0XxYccXbg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+<style>
+    button.btn.btn-outline-secondary {
+        border-radius: 1px;
+        background: #5570F1;
+        color: white;
+        border: 1px solid #5570F1;
+    }
 
-        div#pagination-container {
-            display: flex;
-            justify-content: center;
-            padding: 1%
-        }
+    div#pagination-container {
+        display: flex;
+        justify-content: center;
+        padding: 1%
+    }
 
-        div#card-container {
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center;
-        }
+    .card-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
-        .custom-row {
-            flex-direction: column !important
-        }
+    div#cards-container {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 
-        .main-card {
-            width: 30%;
-            margin: 20px;
-            /* height: 25%; */
-        }
+    .custom-row {
+        flex-direction: column !important
+    }
 
-        /* .main-card .card {
-                                                            height: 100% !important;
-                                                        } */
+    .main-card {
+        width: 30%;
+        margin: 20px;
+        /* height: 25%; */
+    }
 
-        .main-card .card img {
-            height: 240px;
-        }
+    .main-card .card img {
+        height: 240px;
+    }
 
-        .input-group {
-            justify-content: center;
-            gap: 2%;
-            padding: 1%
-        }
+    .input-group {
+        justify-content: center;
+        gap: 2%;
+        padding: 1%
+    }
 
-        .paginationjs .paginationjs-pages li.active>a {
-            background: #5570F1;
-        }
+    .input-group input {
+        width: initial;
+        height: 40px;
+    }
 
-        .loading {
-            height: 33px;
-        }
-    </style>
+    .paginationjs .paginationjs-pages li.active>a {
+        background: #5570F1;
+    }
+
+    .loading {
+        height: 33px;
+    }
+</style>
 @endsection
 
 
 @section('content')
-    <div class="modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content card-detail-modal">
+<div class="modal" tabindex="-1" role="dialog">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content card-detail-modal">
 
-            </div>
         </div>
     </div>
+</div>
 
 
-    <div class="container my-5">
-        <div class="row">
-            {{-- <div class="input-group">
-                <select class="custom-select iso" id="inputGroupSelect04">
-                    <option selected disabled value="">Select Country</option>
-                    @foreach ($countries as $country)
-                        <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
-                    @endforeach
-                </select>
-                <div class="input-group-append">
-                    <button class="btn btn-outline-secondary filter-card" type="button">Submit</button>
-                </div>
-                <img class="loading mx-2 d-none" src="{{ asset('assets/images/loading.gif') }}" alt="">
-            </div> --}}
-        </div>
+<div class="container my-5">
+    <div class="row">
+        {{-- <div class="input-group">
+            <select class="custom-select iso" id="inputGroupSelect04">
+                <option selected disabled value="">Select Country</option>
+                @foreach ($countries as $country)
+                <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
+                @endforeach
+            </select>
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary filter-card" type="button">Submit</button>
+            </div>
+            <img class="loading mx-2 d-none" src="{{ asset('assets/images/loading.gif') }}" alt="">
+        </div> --}}
     </div>
+</div>
 
-    <div class="container">
-        <div class="row custom-row">
-            <div id="card-container">
-                <h3>Please Select Country</h3>
-            </div>
-            <div id="pagination-container">
+<div class="container">
+    <div class="row custom-row">
+        <div class="card-container">
+            <h3>Please Select Country</h3>
 
-            </div>
             <div class="input-group">
                 <select class="custom-select iso" id="inputGroupSelect04">
                     <option selected disabled value="">Select Country</option>
                     @foreach ($countries as $country)
-                        <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
+                    <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
                     @endforeach
                 </select>
                 <div class="input-group-append">
@@ -112,16 +116,33 @@
                 </div>
                 <img class="loading mx-2 d-none" src="{{ asset('assets/images/loading.gif') }}" alt="">
             </div>
+
+            <div class="input-group justify-content-end">
+                <input type="text" placeholder="Search here..." id="search-input">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" id="search" type="button">Search</button>
+                </div>
+                <img class="loading mx-2 d-none" src="{{ asset('assets/images/loading.gif') }}" alt="">
+            </div>
+            <div id="cards-container">
+
+            </div>
+
+            <div id="pagination-container">
+
+            </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('page-script')
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.6.0/pagination.min.js"
-        integrity="sha512-GzbaI5EsNzdEUq6/2XLYpr1y9CUZRIVsUeWTAFgULtQa5jZ3Nug8i0nZKM6jp9NffBCZhymPPQFcF0DK+JkRpw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script>
-        $(document).ready(function() {
+<script src="https://cdnjs.cloudflare.com/ajax/libs/paginationjs/2.6.0/pagination.min.js"
+    integrity="sha512-GzbaI5EsNzdEUq6/2XLYpr1y9CUZRIVsUeWTAFgULtQa5jZ3Nug8i0nZKM6jp9NffBCZhymPPQFcF0DK+JkRpw=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script>
+    let cardsData = [];
+    $(document).ready(function() {
             var isPaginationSet = false;
             $(document).on("click", ".filter-card", function() {
                 let iso = document.querySelector(".iso").value;
@@ -143,6 +164,7 @@
                     },
                     success: function(res) {
                         if (res.status) {
+                            cardsData = res.cards;
                             setPaginationTable(res.cards);
                             element.removeAttribute('disabled');
                             loader.classList.add("d-none")
@@ -156,14 +178,27 @@
                 });
             })
 
-            function setPaginationTable(cards) {
+            $("#search").on("click", function() {
+                let text = document.getElementById("search-input").value;
+                if (!text) {
+                    toastr.error("Type Something to Search");
+                    return
+                }
+                if (!cardsData.length) {
+                    toastr.error("Please Select Country");
+                    return
+                }
+                const filterData = cardsData.filter(item => {
+                   const textString = (`${item.brand} ${item.country_iso}`).toLowerCase();
+                   return textString.includes(text)
+                });
+
                 $('#pagination-container').pagination({
-                    dataSource: cards,
+                    dataSource: filterData,
                     pageSize: 10, // Number of cards to display per page
                     callback: function(data, pagination) {
                         // Clear the card container
-                        $('#card-container').empty();
-
+                        $('#cards-container').empty();
                         // Render cards for the current page's data
                         for (var i = 0; i < data.length; i++) {
                             var card = data[i];
@@ -178,11 +213,38 @@
                         </div>
                     </div>
                     `;
-                            $('#card-container').append(cardHtml);
+                            $('#cards-container').append(cardHtml);
+                        }
+                    }
+                });
+            })
+
+            function setPaginationTable(cards) {
+                $('#pagination-container').pagination({
+                    dataSource: cards,
+                    pageSize: 10, // Number of cards to display per page
+                    callback: function(data, pagination) {
+                        // Clear the card container
+                        $('#cards-container').empty();
+                        // Render cards for the current page's data
+                        for (var i = 0; i < data.length; i++) {
+                            var card = data[i];
+                            var cardHtml = `
+                    <div class="main-card" data-product-id="${card.product_id}">
+                        <div class="card">
+                            <img src="${card.logo_url}" alt="${card.product_id}" />
+                            <h4 class="text-center py-2">${card.brand} ${card.country_iso}</h4>
+                            <div class="d-flex justify-content-center p-3">
+                                <a href="{{ url('gift-card-detail') }}/${card.product_id}" class="btn btn-success view-card-detail" data-product-id="${card.product_id}">Buy Now</a>
+                            </div>
+                        </div>
+                    </div>
+                    `;
+                            $('#cards-container').append(cardHtml);
                         }
                     }
                 });
             }
         })
-    </script>
+</script>
 @endsection
--- resources/views/gift-card-detail.blade.php
+++ resources/views/gift-card-detail.blade.php
@@ -2,203 +2,204 @@
 
 
 @section('title')
-    Card
+Card
 @endsection
 
 @section('css-link')
-    <link rel="stylesheet" href="{{ asset('assets/style/contact.css') }}" />
-    <style>
-        * {
-            font-family: "Lato";
-        }
+<link rel="stylesheet" href="{{ asset('assets/style/contact.css') }}" />
+<style>
+    * {
+        font-family: "Lato";
+    }
 
-        .custom-select {
-            width: 100%;
-            height: 55px;
-            border: 1px solid #8b8b8b;
-        }
+    .custom-select {
+        width: 100%;
+        height: 55px;
+        border: 1px solid #8b8b8b;
+    }
 
-        .alert {
-            position: relative;
-        }
+    .alert {
+        position: relative;
+    }
 
-        button.close.remove-error {
-            background: none;
-            border: none;
-            font-size: 25px;
-            position: absolute;
-            top: 7px;
-            right: 8px;
-            color: #842029;
-            font-weight: bold;
-        }
+    button.close.remove-error {
+        background: none;
+        border: none;
+        font-size: 25px;
+        position: absolute;
+        top: 7px;
+        right: 8px;
+        color: #842029;
+        font-weight: bold;
+    }
 
-        .loading {
-            width: 28px;
-        }
+    .loading {
+        width: 28px;
+    }
 
-        .gift-details {
-            padding-top: 2%;
-            padding-bottom: 2%;
-        }
+    .gift-details {
+        padding-top: 2%;
+        padding-bottom: 2%;
+    }
 
-        .gift-details .gift-card-details {
-            display: flex;
-            justify-content: center;
-            flex-direction: column;
-            gap: 0.5rem
-        }
+    .gift-details .gift-card-details {
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        gap: 0.5rem
+    }
 
-        .gift-details .gift-card-details div img {
-            width: auto;
-            height: 440px;
-        }
+    .gift-details .gift-card-details div img {
+        width: 500px;
+        height: 450px;
+    }
 
-        .gift-details .gift-card-details div .heading {
-            font-size: 18px;
-            font-weight: 900;
-        }
+    .gift-details .gift-card-details div .heading {
+        font-size: 18px;
+        font-weight: 900;
+    }
 
-        .gift-details .gift-card-details div span {
-            font-size: 16px;
-            font-weight: 600;
-        }
+    .gift-details .gift-card-details div span {
+        font-size: 16px;
+        font-weight: 600;
+    }
 
-        .gift-details .recipient-card-details .card {
-            padding: 6%
-        }
+    .gift-details .recipient-card-details .card {
+        padding: 6%
+    }
 
-        .gift-details .recipient-card-details .card form {
-            width: 100%;
-            margin: 0%
-        }
+    .gift-details .recipient-card-details .card form {
+        width: 100%;
+        margin: 0%
+    }
 
-        .gift-details .recipient-card-details .card label {
-            font-size: 16px;
-            font-weight: 600;
-            margin-bottom: 1%
-        }
+    .gift-details .recipient-card-details .card label {
+        font-size: 16px;
+        font-weight: 600;
+        margin-bottom: 1%
+    }
 
-        .gift-details .recipient-card-details .card button {
-            float: right;
-            background-color: #5570F1;
-            color: #ffffff
-        }
-    </style>
+    .gift-details .recipient-card-details .card button {
+        float: right;
+        background-color: #5570F1;
+        color: #ffffff
+    }
+</style>
 @endsection
 
 
 @section('content')
-    <div class="container">
-        <div class="row gift-details">
-            <div class="col-6 gift-card-details align-items-center">
-                <div>
-                    @if (isset($cardDetail->logoUrls) && sizeof($cardDetail->logoUrls) > 0)
-                        <img class="card-logo" src="{{ $cardDetail->logoUrls[0] }}" alt="">
-                    @endif
-                </div>
-                <div>
-                    <span>
-                        {{ $cardDetail->productName }}
-                    </span>
-                </div>
-                <div>
-                    <span>
-                        {{ $cardDetail->brand->brandName }}
-                    </span>
-                </div>
-                <div>
-                    <span class="heading">Amount:</span>
-                    <span>
-                        {{ $cardDetail->denominationType == 'FIXED' ? $cardDetail->fixedRecipientDenominations[0] : $cardDetail->minSenderDenomination[0] }}
-                        {{ $cardDetail->recipientCurrencyCode }}
-                    </span>
-                </div>
+<div class="container">
+    <div class="row gift-details">
+        <div class="col-6 gift-card-details align-items-center">
+            <div>
+                @if (isset($cardDetail->logoUrls) && sizeof($cardDetail->logoUrls) > 0)
+                <img class="card-logo" src="{{ $cardDetail->logoUrls[0] }}" alt="">
+                @endif
             </div>
-            <div class="col-6 recipient-card-details">
-                <div class="card">
-                    @if (Session::has('status') && !session('status'))
-                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                            <strong>Error!</strong> {{ session::get('error') }}
-                            <button type="button" class="close remove-error" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('purchaseGiftCard') }}" id="card-form">
-                        @csrf
-                        <input type="hidden" name="product_id" value="{{ $cardDetail->productId }}">
-                        <input type="hidden" name="product_amount"
-                            value="{{ $cardDetail->denominationType == 'FIXED' ? $cardDetail->fixedRecipientDenominations[0] : $cardDetail->minSenderDenomination[0] }}">
-                        <input type="hidden" name="product_name" value="{{ $cardDetail->productName }}">
-                        <input type="hidden" name="quantity" value="1">
-                        <div class="form-group mb-3">
-                            <label for="email">Recipient Email</label>
-                            <input type="email" class="form-control" name="email" id="email"
-                                aria-describedby="emailHelp" placeholder="Enter Recipient Email">
-                            @error('email')
-                                <small class="form-text text-danger">{{ $msg }}</small>
-                            @enderror
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <label for="phone">Recipient Phone</label>
-                            <input type="text" class="form-control" name="phone" id="phone"
-                                aria-describedby="emailHelp" placeholder="Enter Recipient Phone">
-                            @error('phone')
-                                <small class="form-text text-danger">{{ $msg }}</small>
-                            @enderror
-                        </div>
-
-                        <div class="form-group mb-3">
-                            <label for="phone">Recipient Country</label>
-                            <div>
-                                <select class="custom-select iso" name="country_code" id="inputGroupSelect04">
-                                    <option selected disabled value="">Select Country</option>
-                                    @foreach ($countries as $country)
-                                        <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div>
-                                @error('country_code')
-                                    <small class="form-text text-danger">Please Select Country</small>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group my-3">
-                            <label for="text">Name</label>
-                            <input type="text" class="form-control" name="sender_name" id="sender-name"
-                                aria-describedby="nameHelp" placeholder="Enter Sender Name">
-                            @error('sender_name')
-                                <small class="form-text text-danger">{{ $msg }}</small>
-                            @enderror
-                        </div>
-                        <div class="mb-3">
-                            <label for="card" class="inline-block font-bold mb-2 uppercase text-sm tracking-wider">Enter
-                                Card Detail</label>
-
-                            <div class="bg-gray-100 p-6 rounded-xl">
-                                <div id="card"></div>
-                            </div>
-                        </div>
-
-                        <button type="submit" class="btn">Purchase <img class="loading d-none"
-                                src="{{ asset('assets/images/white-loading.gif') }}" alt=""></button>
-                    </form>
+            <div>
+                <span>
+                    {{ $cardDetail->productName }}
+                </span>
+            </div>
+            <div>
+                <span>
+                    {{ $cardDetail->brand->brandName }}
+                </span>
+            </div>
+            <div>
+                <span class="heading">Amount:</span>
+                <span>
+                    {{ $cardDetail->denominationType == 'FIXED' ? $cardDetail->fixedRecipientDenominations[0] :
+                    $cardDetail->minSenderDenomination[0] }}
+                    {{ $cardDetail->recipientCurrencyCode }}
+                </span>
+            </div>
+        </div>
+        <div class="col-6 recipient-card-details">
+            <div class="card">
+                @if (Session::has('status') && !session('status'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> {{ session::get('error') }}
+                    <button type="button" class="close remove-error" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
+                @endif
+
+                <form method="POST" action="{{ route('purchaseGiftCard') }}" id="card-form">
+                    @csrf
+                    <input type="hidden" name="product_id" value="{{ $cardDetail->productId }}">
+                    <input type="hidden" name="product_amount"
+                        value="{{ $cardDetail->denominationType == 'FIXED' ? $cardDetail->fixedRecipientDenominations[0] : $cardDetail->minSenderDenomination[0] }}">
+                    <input type="hidden" name="product_name" value="{{ $cardDetail->productName }}">
+                    <input type="hidden" name="quantity" value="1">
+                    <div class="form-group mb-3">
+                        <label for="email">Recipient Email</label>
+                        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp"
+                            placeholder="Enter Recipient Email">
+                        @error('email')
+                        <small class="form-text text-danger">{{ $msg }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="phone">Recipient Phone</label>
+                        <input type="text" class="form-control" name="phone" id="phone" aria-describedby="emailHelp"
+                            placeholder="Enter Recipient Phone">
+                        @error('phone')
+                        <small class="form-text text-danger">{{ $msg }}</small>
+                        @enderror
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label for="phone">Recipient Country</label>
+                        <div>
+                            <select class="custom-select iso" name="country_code" id="inputGroupSelect04">
+                                <option selected disabled value="">Select Country</option>
+                                @foreach ($countries as $country)
+                                <option value="{{ $country->iso_name }}">{{ $country->country_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div>
+                            @error('country_code')
+                            <small class="form-text text-danger">Please Select Country</small>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="form-group my-3">
+                        <label for="text">Name</label>
+                        <input type="text" class="form-control" name="sender_name" id="sender-name"
+                            aria-describedby="nameHelp" placeholder="Enter Sender Name">
+                        @error('sender_name')
+                        <small class="form-text text-danger">{{ $msg }}</small>
+                        @enderror
+                    </div>
+                    <div class="mb-3">
+                        <label for="card" class="inline-block font-bold mb-2 uppercase text-sm tracking-wider">Enter
+                            Card Detail</label>
+
+                        <div class="bg-gray-100 p-6 rounded-xl">
+                            <div id="card"></div>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn">Purchase <img class="loading d-none"
+                            src="{{ asset('assets/images/white-loading.gif') }}" alt=""></button>
+                </form>
             </div>
         </div>
     </div>
+</div>
 @endsection
 
 @section('page-script')
-    <script src="https://js.stripe.com/v3/"></script>
-    <script src="{{ asset('assets/plugins/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js') }}"></script>
-    <script>
-        let stripe = Stripe('{{ env('STRIPE_KEY') }}')
+<script src="https://js.stripe.com/v3/"></script>
+<script src="{{ asset('assets/plugins/jQuery-Mask-Plugin-master/dist/jquery.mask.min.js') }}"></script>
+<script>
+    let stripe = Stripe('{{ env('STRIPE_KEY') }}')
         const elements = stripe.elements()
         const cardElement = elements.create('card', {
             style: {
@@ -244,5 +245,5 @@
         $(document).ready(function() {
             $('#phone').mask('(000) 0000-0000');
         })
-    </script>
+</script>
 @endsection
